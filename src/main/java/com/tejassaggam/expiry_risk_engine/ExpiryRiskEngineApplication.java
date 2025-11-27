b7393b50146976a0805031a1b6e6924f610daef7
codeninja333 <tejassaggam1@gmail.com>
2025-11-27 16:30:01 +0530

    C:\Users\tejas>cd

---
+++ src/main/java/com/tejassaggam/expiry_risk_engine/ExpiryRiskEngineApplication.java
@@ -0,0 +1,13 @@
+package com.tejassaggam.expiry_risk_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpiryRiskEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpiryRiskEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tejassaggam/expiry_risk_engine/ExpiryRiskEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejassaggam.expiry_risk_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpiryRiskEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
